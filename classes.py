690828ea40015e004ab2b21d9f670b373e988b7c
Noah Cristino <noahcristino@gmail.com>
2022-04-14 15:23:11 -0400

    created food class

--- classes.py
+++ classes.py
@@ -1,4 +1,12 @@
 class Food:
-  def __init__(self, name, val):
+  def __init__(self, name, fat, cholesterol, calories, sodium, carbs, protein, vits_and_mins):
     self.name = name
-    self.val = val
+    self.fat = fat
+    self.cholesterol = cholesterol
+    self.calories = calories
+    self.sodium = sodium
+    self.carbs = carbs
+    self.protein = protein
+    self.vits_and_mins = vits_and_mins
+  def __str__(self):
+    return self.name + ": calories: " + str(self.calories) + ", fat: "+str(self.fat)+", cholesterol: "+str(self.cholesterol)+", sodium: "+str(self.sodium)+", carbs: "+str(self.carbs)+", protein: "+str(self.protein)+", vitamins and minerals: "+str(self.vits_and_mins)
---
+++ main.py
@@ -0,0 +1,3 @@
+from classes import *
+t = Food("asparagus", 4, 4, 4, 2, 5, 1, 5)
+print(t)
