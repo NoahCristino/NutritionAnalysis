21bf48c1175e733d518f227eb8a0eef2a05fa503
Noah Cristino <noahcristino@gmail.com>
2022-04-15 00:06:32 -0400

    extracted food data from csv

--- classes.py
+++ classes.py
@@ -1,12 +1,12 @@
 class Food:
-  def __init__(self, name, fat, cholesterol, calories, sodium, carbs, protein, vits_and_mins):
+  def __init__(self, name, total_fat, sat_fat, cholesterol, calories, sodium, choline, folate):
     self.name = name
-    self.fat = fat
+    self.total_fat = total_fat
     self.cholesterol = cholesterol
     self.calories = calories
     self.sodium = sodium
-    self.carbs = carbs
-    self.protein = protein
-    self.vits_and_mins = vits_and_mins
+    self.sat_fat = sat_fat
+    self.choline = choline
+    self.folate = folate
   def __str__(self):
-    return self.name + ": calories: " + str(self.calories) + ", fat: "+str(self.fat)+", cholesterol: "+str(self.cholesterol)+", sodium: "+str(self.sodium)+", carbs: "+str(self.carbs)+", protein: "+str(self.protein)+", vitamins and minerals: "+str(self.vits_and_mins)
+    return self.name + ": calories: " + str(self.calories) + ", total_fat: "+str(self.total_fat)+", sat_fat: "+str(self.sat_fat)+", sodium: "+str(self.sodium)+", cholesterol: "+str(self.cholesterol)+", choline: "+str(self.choline)+", folate: "+str(self.folate)
--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 from classes import *
-t = Food("asparagus", 4, 4, 4, 2, 5, 1, 5)
-print(t)
+import csv
+with open('nutrition.csv', newline='') as csvfile:
+     reader = csv.DictReader(csvfile)
+     its = 10
+     it_count = 0
+     for row in reader:
+         total_fat_per_gram = float(row['total_fat'][:-1])*(1/float(row['serving_size'][:-2]))
+         if row['cholesterol'] != '0':
+             cholesterol_per_gram = (float(row['cholesterol'][:-2])/1000)*(1/float(row['serving_size'][:-2]))
+         else:
+             cholesterol_per_gram = 0
+         calories_per_gram = float(row['calories'])*(1/float(row['serving_size'][:-2])) #assumed serving_size is in grams
+         if row['sodium'] != '0':
+             sodium_per_gram = float(row['sodium'][:-2])*(1/float(row['serving_size'][:-2]))
+         else:
+             sodium_per_gram = 0
+         if row['saturated_fat'] != '':
+             sat_fat_per_gram = float(row['saturated_fat'][:-1])*(1/float(row['serving_size'][:-2]))
+         else:
+             sat_fat_per_gram = 0
+         if row['choline'] != '0':
+             choline_per_gram = (float(row['choline'][:-3])/1000)*(1/float(row['serving_size'][:-2]))
+         else:
+             choline_per_gram = 0
+         if row['folate'] != '0':
+             folate_per_gram = (float(row['folate'][:-3])/1000)*(1/float(row['serving_size'][:-2]))
+         else:
+             folate_per_gram = 0
+         f = Food(row['name'], total_fat_per_gram, sat_fat_per_gram, cholesterol_per_gram, calories_per_gram, sodium_per_gram, choline_per_gram, folate_per_gram)
+         #print(f)
+         it_count = it_count + 1
+         if it_count == its:
+             break
